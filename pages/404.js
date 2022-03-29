80be29b92653f4f935c2da75df298ac1022157f8
Kevin Mason <krmason2020@gmail.com>
2022-03-28 21:11:21 -0700

    completed part 8

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,15 @@
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const NotFound = () => {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      //router.go() can basically go back or forwards like this just like the browser arrows
+      router.push("/");
+    }, 3000);
+  }, []);
   return (
     <div className="not-found">
       <h1>Ooooops...</h1>
