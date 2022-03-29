09a55e60583950dfc2872944bfde6e95b01e87da
Kevin Mason <krmason2020@gmail.com>
2022-03-28 18:10:33 -0700

    completed 2nd tutorial

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
+import Footer from "../comps/Footer";
+import Navbar from "../comps/Navbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   //this component is pre rendered, I believe any page is server side rendered
   return (
     <div className={styles.container}>
+      <Navbar />
       <h1>HomePage</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
@@ -19,6 +22,7 @@ export default function Home() {
         voluptatum obcaecati id molestias voluptatibus suscipit laborum
         necessitatibus iure. Aspernatur, quidem laborum?
       </p>
+      <Footer />
     </div>
   );
 }
