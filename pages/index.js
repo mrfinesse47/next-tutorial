2783b882da39ccfd8f8f5d3642d0cbb89e814657
Kevin Mason <krmason2020@gmail.com>
2022-03-28 18:42:13 -0700

    completed part 3

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import Footer from "../comps/Footer";
 import Navbar from "../comps/Navbar";
 import styles from "../styles/Home.module.css";
@@ -22,6 +23,9 @@ export default function Home() {
         voluptatum obcaecati id molestias voluptatibus suscipit laborum
         necessitatibus iure. Aspernatur, quidem laborum?
       </p>
+      <Link href="/ninjas">
+        <a>See Ninja Listing</a>
+      </Link>
       <Footer />
     </div>
   );
