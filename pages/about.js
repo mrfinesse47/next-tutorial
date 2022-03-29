23cd5d8fa0f2e3c1f7d0c8a3823910e22853df3f
Kevin Mason <krmason2020@gmail.com>
2022-03-28 21:40:05 -0700

    completed tut 9

--- pages/about.js
+++ pages/about.js
@@ -1,22 +1,29 @@
 import React from "react";
+import Head from "next/head";
 
 const about = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
-        blanditiis, soluta inventore quis quidem eaque eveniet in quisquam
-        voluptatum obcaecati id molestias voluptatibus suscipit laborum
-        necessitatibus iure. Aspernatur, quidem laborum?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
-        blanditiis, soluta inventore quis quidem eaque eveniet in quisquam
-        voluptatum obcaecati id molestias voluptatibus suscipit laborum
-        necessitatibus iure. Aspernatur, quidem laborum?
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+          blanditiis, soluta inventore quis quidem eaque eveniet in quisquam
+          voluptatum obcaecati id molestias voluptatibus suscipit laborum
+          necessitatibus iure. Aspernatur, quidem laborum?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+          blanditiis, soluta inventore quis quidem eaque eveniet in quisquam
+          voluptatum obcaecati id molestias voluptatibus suscipit laborum
+          necessitatibus iure. Aspernatur, quidem laborum?
+        </p>
+      </div>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -8,23 +8,29 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   //this component is pre rendered, I believe any page is server side rendered
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>HomePage</h1>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
-        blanditiis, soluta inventore quis quidem eaque eveniet in quisquam
-        voluptatum obcaecati id molestias voluptatibus suscipit laborum
-        necessitatibus iure. Aspernatur, quidem laborum?
-      </p>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
-        blanditiis, soluta inventore quis quidem eaque eveniet in quisquam
-        voluptatum obcaecati id molestias voluptatibus suscipit laborum
-        necessitatibus iure. Aspernatur, quidem laborum?
-      </p>
-      <Link href="/ninjas">
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div className={styles.container}>
+        <h1 className={styles.title}>HomePage</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+          blanditiis, soluta inventore quis quidem eaque eveniet in quisquam
+          voluptatum obcaecati id molestias voluptatibus suscipit laborum
+          necessitatibus iure. Aspernatur, quidem laborum?
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+          blanditiis, soluta inventore quis quidem eaque eveniet in quisquam
+          voluptatum obcaecati id molestias voluptatibus suscipit laborum
+          necessitatibus iure. Aspernatur, quidem laborum?
+        </p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   );
 }
