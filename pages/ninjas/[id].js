28c164bc9e3cd77d9861fc1648b44109986d5630
Kevin Mason <krmason2020@gmail.com>
2022-03-29 20:24:45 -0700

    deployed and made a couple notes

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,14 +1,16 @@
 export const getStaticPaths = async () => {
+  //the url
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
   const paths = data.map((ninja) => {
     return { params: { id: ninja.id.toString() } };
   });
 
-  return { paths, fallback: false };
+  return { paths, fallback: false }; //fallback false sends you to the 404 page
 };
 
 export const getStaticProps = async (context) => {
+  //generates pages in .next/server/pages/ on build
   const id = context.params.id;
   const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
   const data = await res.json();
