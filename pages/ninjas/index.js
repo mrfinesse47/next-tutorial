c832796aa4669006525742e5e15b13ccedfd605a
Kevin Mason <krmason2020@gmail.com>
2022-03-29 17:44:24 -0700

    lesson 10 complete

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,9 +1,25 @@
-import React from "react";
+import styles from "../../styles/Ninjas.module.css";
 
-const Ninjas = () => {
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data },
+  };
+}; //you do this instead on useEffect() due to server side rendering
+
+const Ninjas = ({ ninjas }) => {
   return (
     <div>
       <h1>All Ninjas</h1>
+      {ninjas.map((ninja) => (
+        <div key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ninja.name}</h3>
+          </a>
+        </div>
+      ))}
     </div>
   );
 };
